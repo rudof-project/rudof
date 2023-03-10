a68c1409f759db253e6bab38ad15e8077ffb2c62
Jose Emilio Labra Gayo <jelabra@gmail.com>
2023-03-10 09:36:09 +0100

    Playing more with parser...

--- shex_antlr/src/lib.rs
+++ shex_antlr/src/lib.rs
@@ -1,11 +1,16 @@
 mod grammar;
 mod parse_visitor;
 
-
+use std::cell::RefCell;
+use std::rc::Rc;
+use antlr_rust::error_strategy::BailErrorStrategy;
 
 #[cfg(test)]
 mod tests {
+    use crate::grammar::shexdocparser;
     use crate::parse_visitor::ParseVisitor;
+    use crate::parse_visitor::ShExDocErrorListener;
+    use antlr_rust::BailErrorStrategy;
     use antlr_rust::InputStream;
     use antlr_rust::common_token_stream::CommonTokenStream;
     use grammar::{ShExDocLexer, ShExDocParser};
@@ -15,19 +20,39 @@ mod tests {
 
     #[test]
     fn test_parser() {
-        let lexer = ShExDocLexer::new(InputStream::new(
+        let mut lexer = ShExDocLexer::new(InputStream::new(
             r#"<S> {
               <p .  
             }"#.into()));
-        let mut parser = ShExDocParser::new(CommonTokenStream::new(lexer));
-        println!("After parser...Before root...");
-        let root = parser.shExDoc().expect("parse tree root node");
-        println!("After root...Before visitor {:?}", root);
-        let mut visitor = ParseVisitor { schema: SchemaBuilder::new().build() };
-        println!("Afer visitor...before root accept");
-        let v = root.accept(&mut visitor);
-        println!("Result of root accept = {:?}",v);
-        assert_eq!(2+2, 4)
+        let num_errors = Rc::new(RefCell::new(0));
+        let error_listener = Box::new(ShExDocErrorListener::new(Rc::clone(&num_errors)));    
+        lexer.add_error_listener(error_listener);
+        let input = CommonTokenStream::new(lexer);
+        let mut parser = ShExDocParser::new(input);
+        
+        let es = BailErrorStrategy::<'_, shexdocparser::ShExDocParserContextType>::new();
+        
+        // println!("After parser...Before root..., num_errors: {:?}", error_listener.num_errors());
+        //parser.err_handler.try_into()
+        let root = parser.shExDoc(); 
+        match root {
+            Ok(tree) => {
+                println!("After parse...");
+                println!("After root...Before visitor {:?}", tree);
+                let mut visitor = ParseVisitor { schema:   
+                    SchemaBuilder::new(),
+                    errors: Rc::clone(&num_errors)
+                };
+                println!("Afer visitor...before root accept");
+                let v = tree.accept(&mut visitor);
+                println!("Visitor errors = {:?}",visitor.errors);
+                assert!(true, "Result: {:?}",v)
+            }
+            Err(es) => {
+                println!("Found errors...");
+                assert!(false, "Errors found parsing: {:?}",es)
+            }
+        } 
     }
 
 }
--- shex_antlr/src/parse_visitor.rs
+++ shex_antlr/src/parse_visitor.rs
@@ -1,10 +1,15 @@
-use antlr_rust::tree::{ParseTreeVisitor};
+use std::rc::Rc;
+use std::cell::RefCell;
+
+use antlr_rust::{tree::{ParseTreeVisitor}, recognizer::Recognizer, token_factory::TokenFactory, errors::ANTLRError};
 use crate::grammar::{ShExDocVisitor, ShExDocParserContextType, ShExDocContext};
-use shex_ast::Schema;
+use shex_ast::{SchemaBuilder};
+use antlr_rust::error_listener::ErrorListener;
 
 
 pub struct ParseVisitor<'a> {
-    pub schema: Schema<'a>
+    pub schema: SchemaBuilder<'a>,
+    pub errors: Rc<RefCell<u32>>
 }
 
 impl <'a> ParseTreeVisitor<'a, ShExDocParserContextType> for ParseVisitor<'a> {}
@@ -12,7 +17,43 @@ impl <'a> ParseTreeVisitor<'a, ShExDocParserContextType> for ParseVisitor<'a> {}
 impl <'a> ShExDocVisitor<'a> for ParseVisitor<'a> {
     fn visit_shExDoc(&mut self, ctx: &ShExDocContext<'a>) { 
         println!("Visiting shExDoc");
-        self.visit_children(ctx) 
+        self.visit_children(ctx);
     }
 }
 
+#[derive(Debug, Clone)]
+pub struct ShExDocErrorListener {
+    num_errors: Rc<RefCell<u32>>
+}
+
+impl ShExDocErrorListener {
+    pub fn new(counter: Rc<RefCell<u32>>) -> ShExDocErrorListener {
+        ShExDocErrorListener { num_errors: counter }
+    }
+
+    pub fn num_errors(&self) -> u32 {
+        *self.num_errors.borrow()
+    }
+}
+
+impl <'a, T: Recognizer<'a>> ErrorListener<'a, T> for ShExDocErrorListener {
+    fn syntax_error(
+        &self,
+        _recognizer: &T,
+        _offending_symbol: Option<&<T::TF as TokenFactory<'a>>::Inner>,
+        line: isize,
+        column: isize,
+        msg: &str,
+        _e: Option<&ANTLRError>,
+    ) {
+        let mut ne = *self.num_errors.borrow_mut();
+        ne += 1;
+        eprintln!(
+            "line {}:{} Syntax error {} near '{}'",
+            line,
+            column,
+            ne,
+            msg
+        );
+    }
+}
