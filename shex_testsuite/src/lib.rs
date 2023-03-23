8ac1f58986e1dc643a717d66cddcfbba655847f9
Jose Emilio Labra Gayo <jelabra@gmail.com>
2023-03-23 22:25:17 +0100

    Added small serde example

--- shex_testsuite/src/lib.rs
+++ shex_testsuite/src/lib.rs
@@ -1,17 +1,45 @@
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug)]
+struct Manifest {
+    graph: Vec<ManifestGraph>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct ManifestGraph {
+    id: String, 
+    tipe: String, 
+    comment: String,
+    entries: Vec<Entry>
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct Entry {
+    id: String,
+    tipe: String,
+    name: String,
+    status: String,
+    shex: String,
+    json: String,
+    ttl: String
+}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use std::path::Path;
     use std::fs;
 
     #[test]
     fn list_all_files() {
-        const TESTS_FOLDER = "testsuite/tests"
-        let paths = fs::read_dir("./").unwrap();
-        
-        for path in paths {
-         println!("Name: {}", path.unwrap().path().display())
-        }
+        let manifest_path = Path::new("localTest/schemas/manifest_basic.jsonld");
+        let mut manifest = {
+            let manifest_str = fs::read_to_string(&manifest_path).unwrap();
+            serde_json::from_str::<Manifest>(&manifest_str).unwrap()
+        };
 
+        println!("Manifest read: {:?}", manifest);
+        
         assert_eq!(2 + 2, 4);
     }
 }
