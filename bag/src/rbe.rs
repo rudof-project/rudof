1366f608eced2f100d7ee224c4a8788080abfec7
labra <jelabra@gmail.com>
2023-07-16 20:35:46 +0000

    Added more cases for rbe

--- bag/src/rbe.rs
+++ bag/src/rbe.rs
@@ -5,7 +5,7 @@ use std::cmp;
 
 
 
-#[derive(Clone)]
+#[derive(PartialEq, Eq, Clone)]
 enum Rbe<A> {
     Fail { error: RbeError<A> },
     Empty,
@@ -14,14 +14,50 @@ enum Rbe<A> {
     Or { v1: Box<Rbe<A>>, v2: Box<Rbe<A>>},
     Star { v: Box<Rbe<A>>},
     Plus { v: Box<Rbe<A>> },
-    Repeat { v: Box<Rbe<A>>, n: usize, m: Max }
+    Repeat { v: Box<Rbe<A>>, min: usize, max: Max }
 }
 
-impl <A> Rbe<A> {
+
+type NullableResult = bool;
+
+impl <A> Rbe<A> where A: Eq {
     
     
    fn derivBag(&self, bag: Bag<A>, open: bool) -> Rbe<A> {
     todo!()
+   }
+
+   fn nullable(&self) -> NullableResult {
+     match &self {
+        Rbe::Fail{..} => false,
+        Rbe::Empty => true,
+        Rbe::Symbol{value:_ , min: 0, max: _} => true,
+        Rbe::Symbol{value:x, min:min, max: _} => false,
+        Rbe::And{v1, v2} => Self::combineAnd(v1.nullable(),v2.nullable()),
+        Rbe::Or{v1, v2} => Self::combineOr(v1.nullable(),v2.nullable()),
+        Rbe::Star{..} => true,
+        Rbe::Plus{v} => v.nullable(),
+        Rbe::Repeat{v, min: 0, max } => true,
+        Rbe::Repeat{v, min:_, max:_} => v.nullable()
+     }
+   }
+
+   fn combineAnd(v1: NullableResult, v2: NullableResult) -> NullableResult {
+    match (v1,v2) {
+      (true, true) => true,
+      (true, false) => false,
+      (false, true) => false,
+      (false, false) => false
+    }
+   }
+
+   fn combineOr(v1: NullableResult, v2: NullableResult) -> NullableResult {
+    match (v1,v2) {
+      (true, true) => true,
+      (true, false) => true,
+      (false, true) => true,
+      (false, false) => false
+    }
    }
 
    fn deriv(&self, x: &A, open: bool, controlled: &HashSet<A>) -> Rbe<A> 
@@ -60,17 +96,37 @@ impl <A> Rbe<A> {
             }
         },
        Rbe::And{ v1, v2} => { 
-        let d1 = Box::new(v1.deriv(x, open, controlled));
-        let d2 = Box::new(v2.deriv(x, open, controlled));
-        *Self::mkOr(
-            Self::mkAnd(d1, v2.clone()), 
-            Self::mkAnd(d2, v1.clone())
-        )
+        let d1 = v1.deriv(x, open, controlled);
+        let d2 = v2.deriv(x, open, controlled);
+        Self::mkOr(
+          Self::mkAnd(d1, (**v2).clone()), 
+          Self::mkAnd(d2, (**v1).clone())
+        ) 
        },
-       Rbe::Or{ v1, v2 } => { todo!()},
-       Rbe::Plus { v } => { todo!() },
-       Rbe::Repeat { v, n, m } => { todo!() },
-       Rbe::Star { v } => { todo!() }
+       Rbe::Or{ v1, v2 } => { 
+        let d1 = v1.deriv(x, open, controlled);
+        let d2 = v2.deriv(x, open, controlled);
+        Self::mkOr(d1, d2) 
+       },
+       Rbe::Plus { v } => { 
+        let d = v.deriv(x, open, controlled);
+        Self::mkAnd(d, Rbe::Star{ v: v.clone() })
+       },
+       Rbe::Repeat { v, min: 0, max: Max::IntMax(0) } => { 
+        let d = v.deriv(x, open, controlled);
+        if d.nullable() { Rbe::Fail {
+          error: RbeError::CardinalityZeroZeroDeriv{ x: (*x).clone() }
+        }} else {
+          Rbe::Empty
+        }
+       },
+       Rbe::Repeat { v, min: min, max } => { 
+        todo!()
+       },
+       Rbe::Star { v } => { 
+        let d = v.deriv(x, open, controlled);
+        Self::mkAnd(d, (**v).clone())
+       }
      }
    }
 
@@ -91,22 +147,30 @@ impl <A> Rbe<A> {
       }
    }
 
-   fn mkOr(v1: Box<Rbe<A>>, v2: Box<Rbe<A>>) -> Box<Rbe<A>> 
+   fn mkAnd(v1: Rbe<A>, v2: Rbe<A>) -> Rbe<A> 
    where A: Clone {
-    /* match (*v1,*v2) {
-        (Rbe::Empty, e2) => v2,
-        (e1, Rbe::Empty) => v1,
-        (f@Rbe::Fail{..}, _) => v1,
-        (_, f@Rbe::Fail{..}) => v2,
-        (_,_) => Box::new(Rbe::And{ 
-            v1: Box::new((*v1).clone()), 
-            v2: Box::new((*v2).clone()) })
-    } */
-    todo!()
+    match (&v1,&v2) {
+        (Rbe::Empty, _) => v2,
+        (_, Rbe::Empty) => v1,
+        (f@Rbe::Fail{..}, _) => f.clone(),
+        (_, f@Rbe::Fail{..}) => f.clone(),
+        (_, _) => Rbe::And{ 
+            v1: Box::new(v1), 
+            v2: Box::new(v2) 
+        }
+    } 
    }
 
-   fn mkAnd(v1: Box<Rbe<A>>, v2: Box<Rbe<A>>) -> Box<Rbe<A>> {
-    todo!()
+   fn mkOr(v1: Rbe<A>, v2: Rbe<A>) -> Rbe<A> {
+    match (&v1,&v2) {
+        (f@Rbe::Fail{..}, _) => v2,
+        (_, f@Rbe::Fail{..}) => v1,
+        (e1, e2) => if e1 == e2 {
+            v1
+        } else {
+            Rbe::Or{ v1: Box::new(v1), v2: Box::new(v2) }
+        }
+    } 
    }
 
    fn bigger(min: usize, max: &Max) -> bool {
--- bag/src/rbe_error.rs
+++ bag/src/rbe_error.rs
@@ -1,10 +1,11 @@
 use crate::max::Max;
 
-#[derive(Clone)]
+#[derive(PartialEq, Eq, Clone)]
 pub enum RbeError<A> {
    UnexpectedEmpty { x: A, open: bool },
    UnexpectedSymbol { x: A, expected: A, open: bool },
    MaxCardinalityZeroFoundValue { x: A },
    RangeNegativeLowerBound { min: usize },
-   RangeLowerBoundBiggerMax { min: usize, max: Max }
+   RangeLowerBoundBiggerMax { min: usize, max: Max },
+   CardinalityZeroZeroDeriv { x: A }
 }
