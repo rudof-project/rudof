0d6893e69c57b7dcba0d48083cad9f74784a1fcf
Jose Emilio Labra Gayo <jelabra@gmail.com>
2023-08-11 00:17:33 +0200

    Attempt to multi_product

--- rbe/src/rbe_table.rs
+++ rbe/src/rbe_table.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 use std::fmt::Debug;
 use std::fmt::Display;
 use itertools::*;
+use std::vec::IntoIter;
 
 use crate::MatchCond;
 use crate::Pending;
@@ -34,46 +35,46 @@ where K: Hash + Eq + Debug + Display + Default,
         let mut rs = Vec::new();
         for (key, value) in values {
             let conds = self.components.get(&key).unwrap_or_else(|| &empty);
-            let perms = conds.iter().permutations(conds.len());
-            rs.push((value, perms));
+            let mut pairs = Vec::new();
+            for c in conds {
+                pairs.push((value.clone(), (*c).clone()));
+            }
+            rs.push(pairs);
         }
 
-        for (v, ps) in rs {
-            print!("{v} ");
-            for ps in ps {
-                for c in ps {
-                    print!("{c}|");
-                }
-            }
-            println!();
-        }
-        todo!()
-/*         MatchTableIter {
-            state: rs
-        } */
+        let mp: MultiProduct<IntoIter<(V, MatchCond<K, V, R>)>> = rs.into_iter().multi_cartesian_product(); 
+        MatchTableIter {
+            state: mp
+        } 
     }
 }
 
 pub struct MatchTableIter<K,V,R> 
 where K: Hash + Eq + Display + Default,
-      V: Hash + Eq + Default + PartialEq + Clone,
-      R: Default + PartialEq + Clone,
+      V: Hash + Eq + Default + Display + Debug + PartialEq + Clone,
+      R: Default + PartialEq + Display + Debug + Clone,
 {
-    // state: Vec<V, Permutations<Iter<'a, MatchCond<K, V, R>>>>
-    k: K,
-    v: V, 
-    r: R
+    state: MultiProduct<IntoIter<(V, MatchCond<K, V, R>)>>
 }
 
 impl <K, V, R> Iterator for MatchTableIter<K, V, R> 
 where K: Hash + Eq + Display + Default,
-      V: Hash + Eq + Default + PartialEq + Clone,
-      R: Default + PartialEq + Clone,
+      V: Hash + Eq + Default + Display + Debug + PartialEq + Clone,
+      R: Default + PartialEq + Display + Debug + Clone,
 {
     type Item = Pending<V, R>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        todo!()
+        let next_state = self.state.next();
+        match next_state {
+            None => None,
+            Some(vs) => {
+                for (v,cond) in vs {
+                    print!("({v}, {cond})");
+                }
+                Some(Pending::new())
+            }
+        }
     }
 }
 
@@ -83,9 +84,12 @@ mod tests {
 
     #[test]
     fn test_rbe_table_1() {
-        let is_a: MatchCond<char, char, char> = MatchCond::new().with_name("is_a".to_string());
-        let ref_t: MatchCond<char, char, char> = MatchCond::new().with_name("ref_t".to_string());
-        let ref_u: MatchCond<char, char, char> = MatchCond::new().with_name("ref_u".to_string());
+        let is_a: MatchCond<char, char, char> = 
+          MatchCond::new().with_name("is_a".to_string());
+        let ref_t: MatchCond<char, char, char> = 
+          MatchCond::new().with_name("ref_t".to_string());
+        let ref_u: MatchCond<char, char, char> = 
+          MatchCond::new().with_name("ref_u".to_string());
     
         let vs = vec![('p', 'a'), ('q', 'y'), ('q', 'z')];
         let rbe_table:RbeTable<char, char,char> = RbeTable {
@@ -95,11 +99,16 @@ mod tests {
 
         let mut iter = rbe_table.matches(vs);
 
-        assert_eq!(iter.next(), Some(Pending::from(vec![('y', vec!['t']), ('z', vec!['t'])].into_iter())));
-        assert_eq!(iter.next(), Some(Pending::from(vec![('y', vec!['t']), ('z', vec!['u'])].into_iter())));
-        assert_eq!(iter.next(), Some(Pending::from(vec![('y', vec!['u']), ('z', vec!['t'])].into_iter())));
-        assert_eq!(iter.next(), Some(Pending::from(vec![('y', vec!['u']), ('z', vec!['u'])].into_iter())));
-        assert_eq!(iter.next(), None);
+        assert_eq!(iter.next(), 
+          Some(Pending::from(vec![('y', vec!['t']), ('z', vec!['t'])].into_iter())));
+        assert_eq!(iter.next(), 
+          Some(Pending::from(vec![('y', vec!['t']), ('z', vec!['u'])].into_iter())));
+        assert_eq!(iter.next(), 
+          Some(Pending::from(vec![('y', vec!['u']), ('z', vec!['t'])].into_iter())));
+        assert_eq!(iter.next(), 
+          Some(Pending::from(vec![('y', vec!['u']), ('z', vec!['u'])].into_iter())));
+        assert_eq!(iter.next(), 
+          None);
 
     }
 
