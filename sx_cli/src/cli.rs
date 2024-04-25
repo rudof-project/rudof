10f063557af080e325c528908e96430da69a44c4
Marc-Antoine Arnaud <marc-antoine.arnaud@luminvent.com>
2024-04-25 12:00:23 +0200

    chore: format code

--- sx_cli/src/cli.rs
+++ sx_cli/src/cli.rs
@@ -42,9 +42,12 @@ pub enum Command {
         )]
         result_shapemap_format: ShapeMapFormat,
 
-        #[arg(short = 'o', long = "output-file", value_name = "Output file name, default = terminal")]
+        #[arg(
+            short = 'o',
+            long = "output-file",
+            value_name = "Output file name, default = terminal"
+        )]
         output: Option<PathBuf>,
-
     },
 
     Schema {
@@ -67,22 +70,18 @@ pub enum Command {
         )]
         result_schema_format: ShExFormat,
 
-        #[arg(
-            short = 't',
-            long = "show elapsed time",
-            default_value_t = false
-        )]
+        #[arg(short = 't', long = "show elapsed time", default_value_t = false)]
         show_time: bool,
 
-        #[arg(
-            long = "statistics",
-            default_value_t = false
-        )]
+        #[arg(long = "statistics", default_value_t = false)]
         show_statistics: bool,
 
-        #[arg(short = 'o', long = "output-file", value_name = "Output file name, default = terminal")]
+        #[arg(
+            short = 'o',
+            long = "output-file",
+            value_name = "Output file name, default = terminal"
+        )]
         output: Option<PathBuf>,
-
     },
 
     Validate {
@@ -139,9 +138,12 @@ pub enum Command {
         )]
         max_steps: usize,
 
-        #[arg(short = 'o', long = "output-file", value_name = "Output file name, default = terminal")]
+        #[arg(
+            short = 'o',
+            long = "output-file",
+            value_name = "Output file name, default = terminal"
+        )]
         output: Option<PathBuf>,
-
     },
 
     Data {
@@ -156,9 +158,12 @@ pub enum Command {
         )]
         data_format: DataFormat,
 
-        #[arg(short = 'o', long = "output-file", value_name = "Output file name, default = terminal")]
+        #[arg(
+            short = 'o',
+            long = "output-file",
+            value_name = "Output file name, default = terminal"
+        )]
         output: Option<PathBuf>,
-
     },
 
     Node {
@@ -193,9 +198,12 @@ pub enum Command {
         #[arg(short = 'p', long = "predicates")]
         predicates: Vec<String>,
 
-        #[arg(short = 'o', long = "output-file", value_name = "Output file name, default = terminal")]
+        #[arg(
+            short = 'o',
+            long = "output-file",
+            value_name = "Output file name, default = terminal"
+        )]
         output: Option<PathBuf>,
-
     },
 
     Shacl {
@@ -218,11 +226,13 @@ pub enum Command {
         )]
         result_shapes_format: ShaclFormat,
 
-        #[arg(short = 'o', long = "output-file", value_name = "Output file name, default = terminal")]
+        #[arg(
+            short = 'o',
+            long = "output-file",
+            value_name = "Output file name, default = terminal"
+        )]
         output: Option<PathBuf>,
-
     },
-
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
@@ -287,7 +297,7 @@ pub enum DataFormat {
     RDFXML,
     TriG,
     N3,
-    NQuads
+    NQuads,
 }
 
 impl Into<RDFFormat> for DataFormat {
@@ -316,14 +326,13 @@ impl Display for DataFormat {
     }
 }
 
-
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
 #[clap(rename_all = "lower")]
 pub enum ShaclFormat {
     Internal,
     Turtle,
     NTriples,
-    RDFXML, 
+    RDFXML,
     TriG,
     N3,
     NQuads,
--- sx_cli/src/data.rs
+++ sx_cli/src/data.rs
@@ -1,17 +1,17 @@
+use prefixmap::PrefixMap;
 use srdf::srdf_graph::SRDFGraph;
 use srdf::srdf_sparql::SRDFSparql;
-use prefixmap::PrefixMap;
 
 pub enum Data {
     Endpoint(SRDFSparql),
-    RDFData(SRDFGraph)
+    RDFData(SRDFGraph),
 }
 
 impl Data {
     pub fn prefixmap(&self) -> Option<PrefixMap> {
         match self {
             Data::RDFData(data) => Some(data.prefixmap()),
-            Data::Endpoint(_) => None
+            Data::Endpoint(_) => None,
         }
     }
 }
