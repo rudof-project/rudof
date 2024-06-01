2dea58538e68653547cfaf394f7c47a6b1eac867
Jose Emilio Labra Gayo <jelabra@gmail.com>
2024-06-01 23:40:23 +0200

    Repaired errors again

--- dctap/src/dctap_error.rs
+++ dctap/src/dctap_error.rs
@@ -1,7 +1,29 @@
 use thiserror::Error;
 
+use crate::IriS;
 
 #[derive(Error, Debug)]
-pub enum DCTAPError {
+pub enum IriSError {
+    #[error("IRI parse error: {err}")]
+    IriParseError { err: String },
+
+    #[error("Error resolving IRI `{other}` with base IRI `{base}`: {err}")]
+    IriResolveError {
+        err: String,
+        base: IriS,
+        other: IriS,
+    },
 }
 
+impl Clone for IriSError {
+    fn clone(&self) -> Self {
+        match self {
+            IriSError::IriParseError { err } => Self::IriParseError { err: err.clone() },
+            IriSError::IriResolveError { err, base, other } => IriSError::IriResolveError {
+                err: err.clone(),
+                base: base.clone(),
+                other: other.clone(),
+            },
+        }
+    }
+}
--- dctap/src/lib.rs
+++ dctap/src/lib.rs
@@ -9,5 +9,58 @@ pub mod dctap;
 pub mod dctap_error;
 
 pub use dctap::*;
-pub use dctap_error::*;
+pub use iris_error::*;
 
+/// ```
+/// 
+/// #[macro_use]
+/// use iri_s::{IriS, iri};
+/// 
+/// let iri = iri!("http://example.org/");
+/// 
+/// assert_eq!(iri.as_str(), "http://example.org/");
+/// ```
+///
+/// At this moment the implementation leverages on [`oxrdf::NamedNode`](https://docs.rs/oxrdf/latest/oxrdf/struct.NamedNode.html)
+/// 
+/// 
+///
+/// Example
+///
+/// ```
+/// use iri_s::IriS;
+/// use std::str::FromStr;
+///
+/// let iri = IriS::from_str("http://example.org/").unwrap();
+///
+/// assert_eq!(iri.as_str(), "http://example.org/")
+/// ```
+///
+#[macro_export]
+macro_rules! iri {
+ (
+   $lit: tt
+ ) => {
+    IriS::new_unchecked($lit)
+ }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+    use super::*;
+
+    #[test]
+    fn iri_s_test() {
+        let iri1: IriS = IriS::from_str("http://example.org/iri").unwrap();
+        let iri2 = IriS::from_str("http://example.org/iri").unwrap();
+        assert_eq!(iri1, iri2);
+    }
+
+    #[test]
+    fn test_macro() {
+        let iri = iri!("http://example.org/");
+        assert_eq!(iri.as_str(), "http://example.org/")
+    }
+}
