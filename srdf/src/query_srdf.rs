d6c81a99a10a6e4d6f8cf3a70a7200913f9847a9
Jose Emilio Labra Gayo <jelabra@gmail.com>
2024-08-01 13:00:15 +0200

    Added first iteration

--- srdf/src/query_srdf.rs
+++ srdf/src/query_srdf.rs
@@ -1,8 +1,44 @@
+use std::collections::HashMap;
+
+use crate::{SRDFBasic, SRDF};
+
 pub trait QuerySRDF: SRDF {
-    fn query(&self, query: &str) -> Result<impl Iterator<Item<QuerySolution>>, Self::Err>;
+    // Select ?x ?p ?y { where ?x ?p ?y }
+    // Iterator
+    //    HashMap<x -> pepe, p -> loves, y -> ana>
+    //    HashMap<x -> luis, p -> worksFor, y -> Coorp>
+    fn query_select(&self, query: &str) -> Result<QuerySolutionIter<Self>, Self::Err>;
 
-struct QuerySolution {
-    solution: HashMap<String, RDFNode>,
-}
+    fn query_select(&self, query: &str) -> Result<QuerySolutionIter<Self>, Self::Err>;
+
+    fn query_ask(&self, query: &str) -> Result<bool, Self::Err>;
 }
 
+pub struct QuerySolution<T> {
+    map: HashMap<VarName, T>,
+}
+
+impl<T> QuerySolution<T> {
+    pub fn find_solution(&self, name: VarName) -> Option<&T> {
+        self.map.get(&name)
+    }
+}
+
+#[derive(PartialEq, Eq, Debug, Clone, Hash)]
+pub struct VarName {
+    str: String,
+}
+
+pub struct QuerySolutionIter<S>
+where
+    S: SRDFBasic,
+{
+    value: S::Term,
+}
+
+impl<T> Iterator for QuerySolutionIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Result<T>> {
+        todo!()
+    }
+}
