80a9ac2bec4e7e5166c97eefe9bea1741bbd1520
angelip2303 <angel.iglesias.prestamo@gmail.com>
2024-08-12 11:25:27 +0000

    including support for shacl validation

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -11,7 +11,7 @@ pub mod pyshapes {
 
     #[pymodule_init]
     fn pymodule_init(module: &Bound<'_, PyModule>) -> PyResult<()> {
-        module.add("__package__", "pyshapes")?;
+        module.add("__package__", "rudof")?;
         module.add("__version__", env!("CARGO_PKG_VERSION"))?;
         module.add("__author__", env!("CARGO_PKG_AUTHORS").replace(':', "\n"))
     }
