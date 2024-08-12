d9a0054543fed9d490b960d13dcbc86d1d30ab25
angelip2303 <angel.iglesias.prestamo@gmail.com>
2024-08-12 16:38:57 +0000

    now?

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -12,7 +12,7 @@ mod rudof {
     use super::shacl::{parse, validate};
 
     #[pymodule_init]
-    fn pymodule_init(module: &Bound<'_, PyModule>) -> PyResult<()> {
+    fn init(module: &Bound<'_, PyModule>) -> PyResult<()> {
         module.add("__package__", "rudof")?;
         module.add("__version__", env!("CARGO_PKG_VERSION"))?;
         module.add("__author__", env!("CARGO_PKG_AUTHORS").replace(':', "\n"))
