73bd72f9db764ec9fd71114a941fd10f025f95b2
angelip2303 <angel.iglesias.prestamo@gmail.com>
2024-10-02 15:59:12 +0000

    feat: implementing xone

--- shacl_validation/src/constraints/core/logical/xone.rs
+++ shacl_validation/src/constraints/core/logical/xone.rs
@@ -10,17 +10,43 @@ use crate::constraints::Validator;
 use crate::engine::native::NativeEngine;
 use crate::engine::sparql::SparqlEngine;
 use crate::engine::Engine;
+use crate::focus_nodes::FocusNodes;
+use crate::shape::Validate;
+use crate::validation_report::result::ValidationResult;
 use crate::validation_report::result::ValidationResults;
 use crate::value_nodes::ValueNodes;
 
-impl<S: SRDFBasic + 'static> Validator<S> for Xone<S> {
+impl<S: SRDFBasic> Validator<S> for Xone<S> {
     fn validate(
         &self,
-        _store: &S,
-        _engine: impl Engine<S>,
-        _value_nodes: &ValueNodes<S>,
+        store: &S,
+        engine: impl Engine<S>,
+        value_nodes: &ValueNodes<S>,
     ) -> Result<ValidationResults<S>, ConstraintError> {
-        Err(ConstraintError::NotImplemented)
+        let results = value_nodes
+            .iter_value_nodes()
+            .flat_map(move |(focus_node, value_node)| {
+                let valid_shapes_count = self
+                    .shapes()
+                    .iter()
+                    .filter(|shape| {
+                        let focus_nodes = FocusNodes::new(std::iter::once(value_node.clone()));
+                        match shape.validate(store, &engine, Some(&focus_nodes)) {
+                            Ok(results) => results.is_empty(),
+                            Err(_) => false,
+                        }
+                    })
+                    .count();
+
+                if valid_shapes_count != 1 {
+                    Some(ValidationResult::new(focus_node, Some(value_node)))
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<_>>();
+
+        Ok(ValidationResults::new(results.into_iter()))
     }
 }
 
