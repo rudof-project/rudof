3e042a613f2015ae3c915379769122cabcea2d13
angelip2303 <angel.iglesias.prestamo@gmail.com>
2025-03-04 12:32:03 +0100

    fix: removing unnecessary blank lines in the tests

--- shacl_validation/tests/core/complex/mod.rs
+++ shacl_validation/tests/core/complex/mod.rs
@@ -7,18 +7,15 @@ const PATH: &str = "tests/data-shapes/data-shapes-test-suite/tests/core/complex/
 
 #[test]
 fn personexample() -> Result<(), TestSuiteError> {
-    let path = format!("{}/{}.ttl", PATH, "personexample");
-    test::<NativeEngine>(path)
+    let path = format!("{}/{}.ttl", PATH, "personexample");test::<NativeEngine>(path)
 }
 
 #[test]
 fn shacl_shacl_data_shapes() -> Result<(), TestSuiteError> {
-    let path = format!("{}/{}.ttl", PATH, "shacl-shacl-data-shapes");
-    test::<NativeEngine>(path)
+    let path = format!("{}/{}.ttl", PATH, "shacl-shacl-data-shapes");test::<NativeEngine>(path)
 }
 
 #[test]
 fn shacl_shacl() -> Result<(), TestSuiteError> {
-    let path = format!("{}/{}.ttl", PATH, "shacl-shacl");
-    test::<NativeEngine>(path)
+    let path = format!("{}/{}.ttl", PATH, "shacl-shacl");test::<NativeEngine>(path)
 }
