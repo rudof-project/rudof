21298fd5509c524cad825b14576adafce5e39e04
Jose Emilio Labra Gayo <jelabra@gmail.com>
2025-08-13 12:05:32 +0200

    Attempt to update to RDF1.2 in Oxigraph

--- shacl_validation/tests/core/misc/mod.rs
+++ shacl_validation/tests/core/misc/mod.rs
@@ -1,42 +1,50 @@
-use shacl_validation::shacl_processor::ShaclValidationMode;
 // use shacl_validation::Subsetting;
 
-use crate::test;
-use crate::TestSuiteError;
+#[cfg(test)]
+mod tests {
 
-const PATH: &str = "tests/data-shapes/data-shapes-test-suite/tests/core/misc/";
+    use crate::test;
+    use crate::TestSuiteError;
+    use shacl_validation::shacl_processor::ShaclValidationMode;
+    use tracing_test::traced_test;
 
-#[test]
-fn deactivated_001() -> Result<(), TestSuiteError> {
-    let path = format!("{}/{}.ttl", PATH, "deactivated-001");
-    // test(path, ShaclValidationMode::Native, Subsetting::None)
-    test(path, ShaclValidationMode::Native)
-}
+    const PATH: &str = "tests/data-shapes/data-shapes-test-suite/tests/core/misc/";
 
-#[test]
-fn deactivated_002() -> Result<(), TestSuiteError> {
-    let path = format!("{}/{}.ttl", PATH, "deactivated-002");
-    // test(path, ShaclValidationMode::Native, Subsetting::None)
-    test(path, ShaclValidationMode::Native)
-}
+    #[traced_test]
+    #[test]
+    fn deactivated_001() -> Result<(), TestSuiteError> {
+        println!("Running deactivated_001 test");
 
-#[test]
-fn message_001() -> Result<(), TestSuiteError> {
-    let path = format!("{}/{}.ttl", PATH, "message-001");
-    // test(path, ShaclValidationMode::Native, Subsetting::None)
-    test(path, ShaclValidationMode::Native)
-}
+        let path = format!("{}/{}.ttl", PATH, "deactivated-001");
+        // test(path, ShaclValidationMode::Native, Subsetting::None)
+        test(path, ShaclValidationMode::Native)
+    }
 
-#[test]
-fn severity_001() -> Result<(), TestSuiteError> {
-    let path = format!("{}/{}.ttl", PATH, "severity-001");
-    // test(path, ShaclValidationMode::Native, Subsetting::None)
-    test(path, ShaclValidationMode::Native)
-}
+    #[test]
+    fn deactivated_002() -> Result<(), TestSuiteError> {
+        let path = format!("{}/{}.ttl", PATH, "deactivated-002");
+        // test(path, ShaclValidationMode::Native, Subsetting::None)
+        test(path, ShaclValidationMode::Native)
+    }
 
-#[test]
-fn severity_002() -> Result<(), TestSuiteError> {
-    let path = format!("{}/{}.ttl", PATH, "severity-002");
-    // test(path, ShaclValidationMode::Native, Subsetting::None)
-    test(path, ShaclValidationMode::Native)
+    #[test]
+    fn message_001() -> Result<(), TestSuiteError> {
+        let path = format!("{}/{}.ttl", PATH, "message-001");
+        // test(path, ShaclValidationMode::Native, Subsetting::None)
+        test(path, ShaclValidationMode::Native)
+    }
+
+    #[test]
+    fn severity_001() -> Result<(), TestSuiteError> {
+        let path = format!("{}/{}.ttl", PATH, "severity-001");
+        // test(path, ShaclValidationMode::Native, Subsetting::None)
+        test(path, ShaclValidationMode::Native)
+    }
+
+    #[test]
+    fn severity_002() -> Result<(), TestSuiteError> {
+        let path = format!("{}/{}.ttl", PATH, "severity-002");
+        // test(path, ShaclValidationMode::Native, Subsetting::None)
+        test(path, ShaclValidationMode::Native)
+    }
 }
