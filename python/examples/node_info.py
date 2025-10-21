5a19f57e873ef88dc53cbc0aeffd2cbfc94a4461
Jose Labra <jelabra@gmail.com>
2025-10-21 21:44:50 +0200

    Small change

--- python/examples/node_info.py
+++ python/examples/node_info.py
@@ -9,7 +9,7 @@ print(node_info)
 rudof.reset_all()
 
 rudof.use_endpoint("wikidata")
-node_info = rudof.node_info("wd:Q42", ["wdt:P31", "rdfs:label"])
+node_info = rudof.node_info("wd:Q42", ["wdt:P31", "wdt:P19"])
 print(node_info)
 
 endpoints = rudof.list_endpoints()
