ab23f045964a9d61dce288ae215db1cbd88462e1
algarfer <algarferx@proton.me>
2026-02-10 17:21:00 +0100

    chore: rust-fmt now ignores rustemo generated code

--- pgschema/src/parser/mod.rs
+++ pgschema/src/parser/mod.rs
@@ -1,30 +1,21 @@
-#![allow(clippy::all)]
 // Parser for simple Property Graphs
-#[allow(clippy::all)]
 pub mod pg;
 
-#[allow(clippy::all)]
 pub mod pg_actions;
 
-#[allow(clippy::all)]
 pub mod pg_builder;
 
 // Parser for property graph schemas
-#[allow(clippy::all)]
 pub mod pgs;
 
-#[allow(clippy::all)]
 pub mod pgs_actions;
 
 // Builder created for property graph schemas
 pub mod pgs_builder;
 
 // Parser for type maps associations
-#[allow(clippy::all)]
 pub mod map;
 
-#[allow(clippy::all)]
 pub mod map_actions;
 
-#[allow(clippy::all)]
 pub mod map_builder;
