64ebd3bd4c9d1247e2f13c1d4e6e934e8c9012c0
algarfer <algarferx@proton.me>
2026-02-13 12:32:06 +0100

    fix(shacl_ast): wrong output in display trait

--- shacl_ast/src/ast/value.rs
+++ shacl_ast/src/ast/value.rs
@@ -24,7 +24,7 @@ impl Display for Value {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Value::Iri(iri) => write!(f, "value({iri})"),
-            Value::Literal(lit) => write!(f, "value({lit})"),
+            Value::Literal(lit) => write!(f, "literal({lit})"),
         }
     }
 }
