59886638c3fbd11ad7dd1fa0a7463c4a028fced8
algarfer <algarferx@proton.me>
2026-03-20 15:14:31 +0100

    refactor(shacl): moved shacl value to new crate

--- shacl/src/types/mod.rs
+++ shacl/src/types/mod.rs
@@ -1,7 +1,9 @@
 mod shacl_format;
+mod value;
 mod message_map;
 mod severity;
 
 pub(crate) use message_map::MessageMap;
 pub(crate) use severity::Severity;
 pub use shacl_format::ShaclFormat;
+pub(crate) use value::Value;
--- shacl/src/types/value.rs
+++ shacl/src/types/value.rs
@@ -1,11 +1,10 @@
-use std::fmt::Display;
-
 use iri_s::IriS;
 use prefixmap::IriRef;
 use rudof_rdf::rdf_core::term::literal::ConcreteLiteral;
+use std::fmt::{Display, Formatter};
 
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
-pub enum Value {
+pub(crate) enum Value {
     Iri(IriRef),
     Literal(ConcreteLiteral),
 }
@@ -23,7 +22,7 @@ impl From<ConcreteLiteral> for Value {
 }
 
 impl Display for Value {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             Value::Iri(iri) => write!(f, "value({iri})"),
             Value::Literal(lit) => write!(f, "literal({lit})"),
