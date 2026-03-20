a864cc0eb74593fa7622b77161160dc065e0676c
algarfer <algarferx@proton.me>
2026-03-20 15:24:18 +0100

    refactor(shacl): moved shacl ast schema to new crate

--- shacl/src/ast/mod.rs
+++ shacl/src/ast/mod.rs
@@ -1,9 +1,11 @@
 mod component;
 mod node_shape;
 mod property_shape;
+mod schema;
 mod shape;
 pub(crate) mod error;
 
 pub(crate) use component::ASTComponent;
 pub(crate) use node_shape::ASTNodeShape;
 pub(crate) use property_shape::ASTPropertyShape;
+pub(crate) use schema::ASTSchema;
