d9081c613a37b3b10bb82ec9fa8efdb259f2401e
algarfer <algarferx@proton.me>
2026-03-20 14:52:37 +0100

    refactor(shacl): moved shacl ast errors to new crate

--- shacl/src/ast/error.rs
+++ shacl/src/ast/error.rs
@@ -2,7 +2,7 @@ use rudof_rdf::rdf_core::term::Object;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
-pub enum ShaclError {
+pub(crate) enum ASTError {
     #[error("NodeShape has an id which is not an IRI: {id}")]
     NodeShapeIdNotIri { id: Box<Object> },
 
