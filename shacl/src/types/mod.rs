56d2d6309b86d7709752feddef3bcbec66929fa0
algarfer <algarferx@proton.me>
2026-03-20 15:15:38 +0100

    refactor(shacl): moved shacl nodekind to new crate

--- shacl/src/types/mod.rs
+++ shacl/src/types/mod.rs
@@ -1,9 +1,11 @@
 mod shacl_format;
+mod node_kind;
 mod value;
 mod message_map;
 mod severity;
 
 pub(crate) use message_map::MessageMap;
+pub(crate) use node_kind::NodeKind;
 pub(crate) use severity::Severity;
 pub use shacl_format::ShaclFormat;
 pub(crate) use value::Value;
---
+++ shacl/src/types/node_kind.rs
@@ -0,0 +1,24 @@
+use std::fmt::{Display, Formatter};
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub(crate) enum NodeKind {
+    Iri,
+    Lit,
+    BNode,
+    BNodeOrIri,
+    BNodeOrLit,
+    IriOrLit,
+}
+
+impl Display for NodeKind {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            NodeKind::Iri => write!(f, "Iri"),
+            NodeKind::Lit => write!(f, "Literal"),
+            NodeKind::BNode => write!(f, "BlankNode"),
+            NodeKind::BNodeOrIri => write!(f, "BlankNodeOrIri"),
+            NodeKind::BNodeOrLit => write!(f, "BlankNodeOrLiteral"),
+            NodeKind::IriOrLit => write!(f, "IriOrLiteral"),
+        }
+    }
+}
