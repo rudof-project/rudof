67be81cbf05ae407a4887c9900a7bbf460dd5a58
algarfer <algarferx@proton.me>
2026-03-30 14:20:19 +0200

    refactor(shacl): moved ir schema to new crate

--- shacl/src/ir/mod.rs
+++ shacl/src/ir/mod.rs
@@ -8,6 +8,9 @@ mod shape;
 mod node_shape;
 mod property_shape;
 mod dependency_graph;
+mod schema;
 mod reifier_info;
+mod test;
 
 pub(crate) use reifier_info::ReifierInfo;
+pub(crate) use schema::IRSchema;
