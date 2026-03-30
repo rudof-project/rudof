2b482a91b55d1e1d556cf26da3aaa74e5f967291
algarfer <algarferx@proton.me>
2026-03-30 14:23:59 +0200

    fix(shacl): moved aux fns to new crate

--- shacl/src/ir/mod.rs
+++ shacl/src/ir/mod.rs
@@ -1,6 +1,11 @@
 //! SHACL IR (Internal Representation)
 //! Represents SHACL Internal representation which is used for validation
 
+use crate::ir::error::IRError;
+use crate::types::Value;
+use iri_s::IriS;
+use prefixmap::IriRef;
+use rudof_rdf::rdf_core::term::Object;
 
 mod components;
 mod shape_label_idx;
@@ -15,3 +20,23 @@ mod test;
 
 pub(crate) use reifier_info::ReifierInfo;
 pub(crate) use schema::IRSchema;
+
+fn convert_iri_ref(iri_ref: IriRef) -> Result<IriS, IRError> {
+    let iri = iri_ref.get_iri().map_err(|err| {
+        IRError::IriRefConversion {
+            iri_ref: iri_ref.to_string(),
+            err: err.to_string(),
+        }
+    })?;
+
+    Ok(iri.clone())
+}
+
+fn convert_value(value: Value) -> Result<Object, IRError> {
+    let out = match value {
+        Value::Iri(iri_ref) => Object::Iri(convert_iri_ref(iri_ref)?),
+        Value::Literal(lit) => Object::literal(lit),
+    };
+
+    Ok(out)
+}
