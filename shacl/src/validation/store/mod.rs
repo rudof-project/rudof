8bdd064493c32edf4c2cfb36a2144bb7c3f7f283
algarfer <algarferx@proton.me>
2026-04-14 10:45:48 +0200

    fix(shacl): added missing reexport declarations

--- shacl/src/validation/store/mod.rs
+++ shacl/src/validation/store/mod.rs
@@ -2,6 +2,9 @@ mod data_manager;
 mod graph;
 mod sparql;
 
+pub(crate) use graph::Graph;
+pub(crate) use sparql::Endpoint;
+
 pub(crate) trait Store<S> {
     fn store(&self) -> &S;
 }
--- shacl/src/validation/utils/mod.rs
+++ shacl/src/validation/utils/mod.rs
@@ -1,3 +1,7 @@
 mod error;
 mod constraint;
 mod sparql;
+
+pub(crate) use error::{SparqlError, SrdfError};
+pub(crate) use constraint::{validate_ask_with, validate_with, validate_with_focus};
+pub(crate) use sparql::select;
