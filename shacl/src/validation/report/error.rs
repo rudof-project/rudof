edd7514b103ed01e8d6c77f581415ab3146fc071
algarfer <algarferx@proton.me>
2026-04-14 11:14:02 +0200

    refactor(shacl): moved shacl validation report error to new crate

---
+++ shacl/src/validation/report/error.rs
@@ -0,0 +1,66 @@
+use thiserror::Error;
+use crate::validation::utils::SrdfError;
+
+#[derive(Debug, Error)]
+pub(crate) enum ReportError {
+
+    #[error("Obtaining objects for subject {subject} with predicate {predicate}: {error}")]
+    ObjectsFor {
+        subject: String,
+        predicate: String,
+        error: String
+    },
+
+    #[error("Error parsing the Validation Report: {error}")]
+    Srdf {
+        #[from]
+        error: SrdfError,
+    },
+
+    #[error(transparent)]
+    Result (#[from] ResultError),
+
+    #[error("Error generating Validation Report: {msg}")]
+    ValidationError {
+        msg: String
+    },
+
+}
+
+#[derive(Debug, Error)]
+pub(crate) enum ResultError {
+    #[error("Obtaining path for subject {subject}: {error}")]
+    PathFor {
+        subject: String,
+        error: String,
+    },
+
+    #[error("Obtaining objects for subject {subject} with predicate {predicate}: {error}")]
+    ObjectFor {
+        subject: String,
+        predicate: String,
+        error: String,
+    },
+
+    #[error("Error parsing the Validation Result, the {field} field is missing")]
+    MissingRequiredField {
+        field: String
+    },
+
+    #[error("Error parsing the Validation Result: {err}")]
+    Srdf {
+        err: SrdfError,
+    },
+
+    #[error("Error parsing the Validation Result, the field '{field}' has an invalid IRI value: {value}")]
+    WrongIriForSeverity {
+        field: String,
+        value: String,
+    },
+
+    #[error("Error parsing the Validation Result, the field '{field}' has an invalid IRI value: {value}")]
+    WrongNodeForSeverity {
+        field: String,
+        value: String
+    },
+}
