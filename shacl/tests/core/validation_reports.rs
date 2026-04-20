010b0536e61ca4298484a6744a90cd4b1702176a
algarfer <algarferx@proton.me>
2026-04-20 14:20:11 +0200

    test(shacl): removed non tests cases

--- shacl/tests/core/validation_reports.rs
+++ shacl/tests/core/validation_reports.rs
@@ -8,18 +8,6 @@ mod tests {
     const PATH: &'static str = "tests/data-shapes/data-shapes-test-suite/tests/core/validation-reports/";
 
     #[test]
-    fn shared_data() -> Result<(), TestSuiteError> {
-        let path = format!("{}/{}.ttl", PATH, "shared-data");
-        test(path, ShaclValidationMode::Native)
-    }
-
-    #[test]
-    fn shared_shapes() -> Result<(), TestSuiteError> {
-        let path = format!("{}/{}.ttl", PATH, "shared-shapes");
-        test(path, ShaclValidationMode::Native)
-    }
-
-    #[test]
     #[ignore]
     fn shared() -> Result<(), TestSuiteError> {
         let path = format!("{}/{}.ttl", PATH, "shared");
