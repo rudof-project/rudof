6bbc99d6c08ce7c27295a11e85d0478dbf14d8ff
algarfer <algarferx@proton.me>
2026-05-06 11:13:03 +0200

    fix(shacl): moved defined_properties_for fn to ast module

--- shacl/src/ast/mod.rs
+++ shacl/src/ast/mod.rs
@@ -11,9 +11,38 @@ mod reifier_info;
 mod schema;
 mod shape;
 
+use std::collections::HashSet;
 pub use component::ASTComponent;
 pub use node_shape::ASTNodeShape;
 pub use property_shape::ASTPropertyShape;
 pub use reifier_info::ReifierInfo;
+use rudof_iri::IriS;
+use rudof_rdf::rdf_core::term::Object;
 pub use schema::ASTSchema;
 pub use shape::ASTShape;
+use crate::error::ASTError;
+
+pub fn defined_properties_for(properties: &[Object], ast: &ASTSchema) -> Result<HashSet<IriS>, ASTError> {
+    let mut defined_properties = HashSet::new();
+    for prop_shape_ref in properties {
+        let prop_shape = ast.get_shape(prop_shape_ref).ok_or_else(|| prop_shape_ref.clone())?;
+        match prop_shape {
+            ASTShape::PropertyShape(ps) => {
+                // Better to ignore complex paths then make Rudof crash
+                // Also, paths like [ sh:inversePath ex:path ] should be ignored, as that does not
+                // add an expected property
+                if let Some(pred) = ps.path().pred() {
+                    defined_properties.insert(pred.clone());
+                }
+            },
+            _ => {
+                return Err(ASTError::UnexpectedShapeType {
+                    expected: "PropertyShape".to_string(),
+                    shape: Box::new(prop_shape_ref.clone()),
+                });
+            },
+        }
+    }
+
+    Ok(defined_properties)
+}
