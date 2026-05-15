d7b82ff902d54a8ce86f40f2e726fbaafea565ab
algarfer <algarferx@proton.me>
2026-05-15 11:22:42 +0200

    feat(nix): added shell hook and pre-commit dev dependency

--- nix/devshells.nix
+++ nix/devshells.nix
@@ -4,10 +4,14 @@
   ...
 }: {
   default = common.craneLib.devShell {
+    shellHook = ''
+      echo "rudof development shell loaded"
+    '';
     packages = with pkgs; [
       common.toolchain
       cargo-edit
       openssl
+      pre-commit
     ];
   };
 }
