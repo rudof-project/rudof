771a8cde1db4cd5d30fe8634403122ae8ba5624c
algarfer <algarferx@proton.me>
2026-05-29 09:22:13 +0200

    feat(shex_ast): improved ShExFormat serde

--- shex_ast/src/shex_format.rs
+++ shex_ast/src/shex_format.rs
@@ -1,11 +1,11 @@
 use std::fmt::{Display, Formatter};
-
+use std::str::FromStr;
 use rudof_iri::MimeType;
 use rudof_rdf::rdf_core::RDFFormat;
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
 /// Contains possible ShEx formats
-#[derive(Deserialize, Serialize, Debug, PartialEq, Clone, Default)]
+#[derive(Debug, PartialEq, Clone, Default)]
 pub enum ShExFormat {
     #[default]
     ShExC,
@@ -29,6 +29,40 @@ impl MimeType for ShExFormat {
             ShExFormat::ShExC => "text/shex",
             ShExFormat::ShExJ => "application/shex+json",
             ShExFormat::RDFFormat(rdf_format) => rdf_format.mime_type(),
+        }
+    }
+}
+
+impl Serialize for ShExFormat {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer
+    {
+        serializer.serialize_str(&self.to_string().to_lowercase())
+    }
+}
+
+impl<'de> Deserialize<'de> for ShExFormat {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>
+    {
+        let str = String::deserialize(deserializer)?;
+        ShExFormat::from_str(&str)
+            .map_err(serde::de::Error::custom)
+    }
+}
+
+impl FromStr for ShExFormat {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "shexc" | "shex" => Ok(ShExFormat::ShExC),
+            "shexj" | "json" => Ok(ShExFormat::ShExJ),
+            _ => RDFFormat::from_str(s)
+                .map(ShExFormat::RDFFormat)
+                .map_err(|_| format!("Unknown ShEx format: {s}")),
         }
     }
 }
