05d31922065efcadd291c42d6e55266ab901da56
algarfer <algarferx@proton.me>
2026-06-01 11:20:55 +0200

    fix(shapes_converter): fixed default ShEx2UmlConfig annotation label value

--- shapes_converter/src/shex_to_uml/shex2uml_config.rs
+++ shapes_converter/src/shex_to_uml/shex2uml_config.rs
@@ -138,7 +138,7 @@ impl ShEx2UmlConfig {
 #[allow(dead_code)]
 impl ShEx2UmlConfig {
     #[inline] fn default_plantuml_path() -> Option<PathBuf> { None }
-    #[inline] fn default_annotation_label() -> Vec<IriS> { Vec::default() }
+    #[inline] fn default_annotation_label() -> Vec<IriS> { vec![RdfsVocab::rdfs_label()] }
     #[inline] fn default_replace_iri_by_label() -> bool { true }
     #[inline] fn default_shadowing() -> bool { true }
     #[inline] fn default_line_type() -> LineType { LineType::default() }
