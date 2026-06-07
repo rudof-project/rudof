9f4887751f1658412f757eb7d5d2a6a7e2706b46
algarfer <algarferx@proton.me>
2026-06-07 18:42:39 +0200

    feat(dctap): improved PlaceholderResolver serde

--- dctap/src/placeholder_resolver.rs
+++ dctap/src/placeholder_resolver.rs
@@ -2,12 +2,17 @@ use serde::{Deserialize, Serialize};
 
 /// Different types of Placeholder resolvers
 #[derive(Deserialize, Serialize, Debug, PartialEq, Clone)]
+#[serde(rename_all = "snake_case")]
 pub enum PlaceholderResolver {
     /// Stem resolver simply adds the generated id to the stem
-    Stem { stem: String },
+    Stem { #[serde(flatten)] stem: String },
 }
 
 impl PlaceholderResolver {
+    pub fn new() -> Self {
+        Self::stem("")
+    }
+
     pub fn stem(stem: &str) -> PlaceholderResolver {
         PlaceholderResolver::Stem { stem: stem.to_string() }
     }
@@ -21,6 +26,6 @@ impl PlaceholderResolver {
 
 impl Default for PlaceholderResolver {
     fn default() -> Self {
-        PlaceholderResolver::Stem { stem: "".to_string() }
+        Self::new()
     }
 }
