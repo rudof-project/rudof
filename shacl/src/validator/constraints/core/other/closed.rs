83e1bb699303564f46ab866bfb2cd452084f8d80
algarfer <algarferx@proton.me>
2026-06-25 15:04:12 +0200

    fix(shacl): fixed SPARQL engine, value and other constraints

--- shacl/src/validator/constraints/core/other/closed.rs
+++ shacl/src/validator/constraints/core/other/closed.rs
@@ -5,7 +5,7 @@ use crate::validator::constraints::NativeValidator;
 use crate::validator::engine::Engine;
 use crate::validator::nodes::ValueNodes;
 use crate::validator::report::ValidationResult;
-use rudof_rdf::rdf_core::term::{Object, Triple};
+use rudof_rdf::rdf_core::term::{Object, Term, Triple};
 use rudof_rdf::rdf_core::{NeighsRDF, SHACLPath};
 use std::fmt::Debug;
 
@@ -101,29 +101,56 @@ impl<S: QueryRDF + NeighsRDF + Debug + 'static> BasicSparqlValidator<S> for Clos
         for (fnode, _) in value_nodes.iter() {
             let focus_obj = S::term_as_object(fnode)?;
 
-            let query = formatdoc! {"
-                SELECT ?p ?o WHERE {{ {} ?p ?o .{} }}
-            ", fnode, not_in_clause};
-
-            let solutions = store
-                .query_select(&query)
-                .map_err(ValidationError::select_query_error::<S>)?;
-
-            for sol in solutions.iter() {
-                let pred_term = match sol.find_solution("p") {
-                    Some(t) => t,
-                    None => continue,
+            let triples_iter: Box<dyn Iterator<Item = (Object, Option<Object>)>> = if fnode.is_blank_node() {
+                let subject = match S::term_as_subject(fnode) {
+                    Ok(s) => s,
+                    Err(_) => continue,
                 };
-                let pred_iri = match S::term_as_object(pred_term) {
-                    Ok(Object::Iri(iri)) => iri,
-                    _ => continue,
+                let triples = store
+                    .triples_with_subject(&subject)
+                    .map_err(ValidationError::new_graph_error::<S>)?;
+                Box::new(triples.into_iter().filter_map(|t| {
+                    let (_, pred, obj) = t.into_components();
+                    let pred_iri = pred.into();
+                    if allowed_props.contains(&pred_iri) {
+                        None
+                    } else {
+                        Some((Object::Iri(pred_iri), S::term_as_object(&obj).ok()))
+                    }
+                }))
+            } else {
+                let query = formatdoc! {"
+                    SELECT ?p ?o WHERE {{ {} ?p ?o .{} }}
+                ", fnode, not_in_clause};
+
+                let solutions = store
+                    .query_select(&query)
+                    .map_err(ValidationError::select_query_error::<S>)?;
+
+                let mut rows: Vec<(Object, Option<Object>)> = Vec::new();
+                for sol in solutions.iter() {
+                    let pred_term = match sol.find_solution("p") {
+                        Some(t) => t,
+                        None => continue,
+                    };
+                    let pred_obj = match S::term_as_object(pred_term) {
+                        Ok(obj @ Object::Iri(_)) => obj,
+                        _ => continue,
+                    };
+                    let value = sol.find_solution("o").and_then(|t| S::term_as_object(t).ok());
+                    rows.push((pred_obj, value));
+                }
+                Box::new(rows.into_iter())
+            };
+
+            for (pred_obj, value) in triples_iter {
+                let path = match &pred_obj {
+                    Object::Iri(iri) => Some(SHACLPath::iri(iri.clone())),
+                    _ => None,
                 };
-
-                let value = sol.find_solution("o").and_then(|t| S::term_as_object(t).ok());
-
                 let vr = ValidationResult::new(focus_obj.clone(), component_obj.clone(), shape.severity().clone())
                     .with_source(Some(shape.id().clone()))
-                    .with_path(Some(SHACLPath::iri(pred_iri)))
+                    .with_path(path)
                     .with_value(value);
                 results.push(vr);
             }
