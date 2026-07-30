2923dae173ff22b8dc327ad20922536842072b8d
algarfer <algarferx@proton.me>
2026-07-30 13:35:05 +0200

    fix(shapes_converter): wrong Default implementation for Shacl2ShexConfig

--- shapes_converter/src/shacl_to_shex/shacl2shex_config.rs
+++ shapes_converter/src/shacl_to_shex/shacl2shex_config.rs
@@ -4,7 +4,7 @@ use rudof_rdf::rdf_core::RdfDataConfig;
 use shacl::validator::ShaclConfig;
 
 /// Defines the configuration of the converter
-#[derive(Deserialize, PartialEq, Debug, Clone, Default)]
+#[derive(Deserialize, PartialEq, Debug, Clone)]
 pub struct Shacl2ShExConfig {
     /// Starting shapes mode. Default: NonBNodes
     #[serde(rename = "starting_shapes_mode", default = "Shacl2ShExConfig::default_starting_shapes_mode")]
@@ -16,7 +16,7 @@ pub struct Shacl2ShExConfig {
 
     /// SHACL configuration
     #[cfg(not(target_family = "wasm"))]
-    #[serde(rename = "shacl", default = "Shacl2ShExConfig::default_shacl_config")]
+    #[serde(rename = "shacl", default = "Shacl2ShExConfig::default_shacl")]
     pub(crate) shacl: ShaclConfig,
 
     /// Add an `rdf:type` constraint for `sh:targetClass` declarations
