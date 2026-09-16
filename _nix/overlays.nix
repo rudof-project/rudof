9d7cd139b0e9fb3173f9d04d487c9ebe13584904
algarfer <algarferx@proton.me>
2026-09-16 14:10:31 +0200

    fix(nix): overlay no longer needed

--- _nix/overlays.nix
+++
@@ -1,8 +0,0 @@
-{self, ...}: {
-  default = final: prev: {
-    rudof = {
-      rudof = self.packages.${final.system}.rudof;
-      rudof-generate = self.packages.${final.system}.rudof-generate;
-    };
-  };
-}
