59f2fd2127d33c653d981629d0ceabb7e2920257
algarfer <algarferx@proton.me>
2026-09-16 14:55:26 +0200

    fix(nix): fixed fenix overlay

--- nix/fenix.nix
+++ nix/fenix.nix
@@ -6,9 +6,12 @@
     };
   };
 
-  nixpkgs.overlays = [ inputs.fenix.overlays.default ];
+  perSystem = { system, inputs', ... }: {
+    _module.args.pkgs = import inputs.nixpkgs {
+      inherit system;
+      overlays = [ inputs.fenix.overlays.default ];
+    };
 
-  perSystem = { inputs', ... }: {
     packages.toolchain = inputs'.fenix.packages.stable.withComponents [
       "cargo"
       "rustc"
